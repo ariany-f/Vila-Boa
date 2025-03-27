52b0778ea5c67c1e822ac7c5fc008564471d927c
ariany-f <ariany_f@hotmail.com>
2025-03-27 12:00:45 -0300

    Ajuste entrypoint

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -11,9 +11,9 @@ chmod -R 777 logs
 chmod -R 777 tmp
 
 # Executa migrações do banco de dados se necessário
-if [ -f "bin/cake.php" ]; then
-    bin/cake.php migrations migrate || true
-fi
+# if [ -f "bin/cake.php" ]; then
+#     bin/cake.php migrations migrate || true
+# fi
 
 # Inicia o servidor PHP embutido
-exec php -S 0.0.0.0:8080 -t public
+exec php -S 0.0.0.0:8080 -t webroot
