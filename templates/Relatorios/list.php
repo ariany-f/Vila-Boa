4153cf89bc3112baf0b5c7728ef7c6e0eed94ac4
ariany-f <ariany_f@hotmail.com>
2025-03-31 17:10:26 -0300

    link em relatorios

--- templates/Relatorios/list.php
+++ templates/Relatorios/list.php
@@ -1,74 +1,82 @@
-<div class="row gy-4">
-    <div class="col-xxl-12">
-        <div class="card h-100 p-0 email-card">
-            <div class="card-header border-bottom bg-base py-16 px-24">
-                <div class="d-flex flex-wrap align-items-center justify-content-between gap-4">
-                    <div class="d-flex align-items-center gap-3">
-                        <div class="form-check style-check d-flex align-items-center">
-                            <input class="form-check-input radius-4 border input-form-dark" type="checkbox" name="checkbox" id="selectAll">
-                        </div>
-                        <button type="button" class="delete-button d-none text-secondary-light text-xl d-flex">
-                            <iconify-icon icon="material-symbols:delete-outline" class="icon line-height-1"></iconify-icon>
-                        </button>
-                        <button type="button" class="reload-button text-secondary-light text-xl d-flex">
-                            <iconify-icon icon="tabler:reload" class="icon"></iconify-icon>
-                        </button>
-                        <form class="navbar-search d-lg-block d-none">
-                            <input type="text" class="bg-base h-40-px w-auto" name="search" id="searchInput" placeholder="Search">
-                            <iconify-icon icon="ion:search-outline" class="icon"></iconify-icon>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="card-body p-0">
-                <ul id="relatorioList" class="overflow-x-auto">
-                    <?php foreach ($relatorios as $relatorio): ?>
-                        <li class="email-item px-24 py-16 gap-4 align-items-center border-bottom cursor-pointer bg-hover-neutral-200 min-w-max-content "
-                        style="display: flex;" 
-                        data-titulo="<?= h($relatorio->titulo) ?>"> 
-                            <div class="form-check style-check d-flex align-items-center">
-                                <input class="form-check-input radius-4 border border-neutral-400" type="checkbox" name="checkbox">
-                            </div>
-                            <a target="_blank" href="<?= h($relatorio->link_iframe) ?>" 
-                                class="text-primary-light fw-medium text-md text-line-1 w-190-px">
-                                <?= h($relatorio->titulo) ?>
-                            </a>
-
-                            <a target="_blank" href="<?= h($relatorio->link_iframe) ?>" 
-                                class="text-primary-light fw-medium mb-0 text-line-1 max-w-740-px">
-                                <?= h($relatorio->link_iframe) ?>
-                            </a>
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
-            </div>
-        </div>
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var iterable<\App\Model\Entity\Relatorio> $relatorios
+ */
+?>
+<div class="card h-100 p-0 radius-12">
+    <div class="card-header border-bottom bg-base py-16 px-24 d-flex align-items-center flex-wrap gap-3 justify-content-end">
+        <button type="button" class="btn btn-primary text-sm btn-sm px-12 py-12 radius-8 d-flex align-items-center gap-2" data-bs-toggle="modal" data-bs-target="#addRelatorioModal">
+            <iconify-icon icon="ic:baseline-plus" class="icon text-xl line-height-1"></iconify-icon>
+            Adicionar Relatório
+        </button>
     </div>
+
+    <div class="card-body p-24">
+        <div class="table-responsive scroll-sm">
+            <table class="table bordered-table sm-table mb-0" id="relatoriosTable">
+                <thead>
+                    <tr>
+                        <th scope="col">
+                            <div class="d-flex align-items-center gap-10">
+                                <div class="form-check style-check d-flex align-items-center">
+                                    <input class="form-check-input radius-4 border input-form-dark" type="checkbox" name="checkbox" id="selectAll">
+                                </div>
+                                ID
+                            </div>
+                        </th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Descrição</th>
+                        <th scope="col" class="text-center">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($relatorios as $relatorio): ?>
+                        <tr>
+                            <td>
+                                <div class="d-flex align-items-center gap-10">
+                                    <div class="form-check style-check d-flex align-items-center">
+                                        <input class="form-check-input radius-4 border border-neutral-400" type="checkbox" name="checkbox">
+                                    </div>
+                                    <?= h($relatorio->id) ?>
+                                </div>
+                            </td>
+                            <td><?= h($relatorio->titulo) ?></td>
+                            <td>
+                                <p class="max-w-500-px"><?= h($relatorio->descricao) ?></p>
+                            </td>
+                            <td class="text-center">
+                                <div class="d-flex align-items-center gap-10 justify-content-center">
+                                    <a target="_blank" href="<?= $this->Url->build(['action' => 'view', $relatorio->id]) ?>" class="bg-success-focus text-success-600 bg-hover-success-200 fw-medium w-40-px h-40-px d-flex justify-content-center align-items-center rounded-circle">
+                                        <iconify-icon icon="mage:external-link" class="menu-icon"></iconify-icon>
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+        
+        <?= $this->Flash->render() ?>
+    </div>
+    <!-- Renderiza o conteúdo do add.php dentro do index.php -->
+    <?= $this->element('Relatorios/add', ['relatorio' => $relatorio]) ?>
 </div>
 
 
-
 <?php $this->start('script'); ?>
-    <script>
+<script>
+    $(document).ready(function() {
+        // $('#relatoriosTable').DataTable({
+        //     paging: true,
+        //     ordering: true,
+        //     info: true
+        // });
 
-    // Search functionality
-    $('#searchInput').on('input', function () {
-        var searchQuery = $(this).val().toLowerCase();  // Pega o texto da pesquisa
-       
-        $('#relatorioList .email-item').each(function () {
-            var titulo = $(this).data('titulo').toLowerCase();  // Pega o título de cada item
-            if (titulo.indexOf(searchQuery) !== -1) {
-                $(this).attr('style', 'display: flex!important;');  // Mostra o item se ele corresponder à pesquisa
-            } else {
-                $(this).attr('style', 'display: none!important;'); 
-            }
+        $('.remove-item-btn').on('click', function () {
+            $(this).closest('tr').addClass('d-none');
         });
     });
-
-    // Page Reload Js
-    $('.reload-button').on('click', function () {
-        history.go(0);
-    });
-
 </script>
 <?php $this->end(); ?>
